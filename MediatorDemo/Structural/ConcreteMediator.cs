dd31a59bccad10f649627823563119636780df74
Rachana Acharya <racharya@paylocity.com>
2019-12-29 12:00:39 -0700

    Add concrete mediator class

---
+++ MediatorDemo/Structural/ConcreteMediator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediatorDemo.Structural
+{
+  public class ConcreteMediator : Mediator
+  {
+    public Collegue1 Collegue1 { get; set; }
+    public Collegue2 Collegue2 { get; set; }
+
+    public override void Send(string message, Colleague colleague)
+    {
+      if(colleague == this.Collegue1)
+      {
+        this.Collegue1.HandleNotification(message);
+      }
+      else
+      {
+        this.Collegue2.HandleNotification(message);
+      }
+    }
+  }
+}
