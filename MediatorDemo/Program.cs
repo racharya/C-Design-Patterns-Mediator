9e388621354353a8fefa93b795bf94b6f23d954c
Rachana Acharya <racharya@paylocity.com>
2019-12-29 12:12:58 -0700

    Use the mediator pattern

--- MediatorDemo/Program.cs
+++ MediatorDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MediatorDemo.Structural;
 
 namespace MediatorDemo
 {
@@ -6,7 +7,14 @@ namespace MediatorDemo
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      var mediator = new ConcreteMediator();
+      var c1 = new Collegue1(mediator); // collegues referencing mediator
+      var c2 = new Collegue2(mediator);
+      mediator.Collegue1 = c1; // letting mediator know about its collegues
+      mediator.Collegue2 = c2;
+
+      c1.Send("Hello, World! (from c1)");
+      c2.Send("Hello, World! (from c2)");
     }
   }
 }
