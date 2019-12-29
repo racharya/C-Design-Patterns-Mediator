c8a3ed0ecd89288ead82bae75c3bd5a574c8c83f
Rachana Acharya <racharya@paylocity.com>
2019-12-29 12:03:11 -0700

    Add concrete colleagues

---
+++ MediatorDemo/Structural/Collegue1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediatorDemo.Structural
+{
+  public class Collegue1: Colleague
+  {
+
+    public Collegue1(Mediator mediator): base(mediator)
+    {
+    }
+
+    public override void HandleNotification(string message)
+    {
+      Console.WriteLine($"Collegue1 receives notification message: {message}");
+    }
+  }
+}
